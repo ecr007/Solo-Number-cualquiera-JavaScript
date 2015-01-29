920526e43244cdd6cdb6d95246fe0c1ab682e12f
Ever <evercuevas1000@gmail.com>
2015-01-29 14:07:46 -0400

    Create onlyNumber.js

---
+++ onlyNumber.js
@@ -0,0 +1,23 @@
+function soloNumeros(event){
+  
+  //Aqui se colocan las teclas que quedaran fuera.
+  if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 110){}
+        
+  //Las que no se permiten.
+  else{
+    if(event.keyCode < 95){
+      if(event.keyCode < 48 || event.keyCode > 57 ){
+        event.preventDefault(); 
+      }
+    }else{
+      if(event.keyCode < 96 || event.keyCode > 105){
+        event.preventDefault(); 
+      }
+    }
+  }
+}
+
+//Get Code de las letras
+$("#textinput").keydown(function(e) {
+  console.log(e.keyCode); // this value
+});
